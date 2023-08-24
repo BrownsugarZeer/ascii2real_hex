7a0410eac4b8df8d7d084db205f43fe61237e13e
jackzewu <Jack.ZE.Wu@liteon.com>
2023-08-24 09:01:09 +0800

    modulize the code

--- ascii2hex.py
+++ ascii2hex.py
@@ -1,7 +1,11 @@
-source = "pwr-200-wvd-5412-of-pm-New_Firmware.hex"
-output = "output2.bin"
+def convert_hex_to_bin(source_path: str, output_path: str) -> None:
+    with open(output_path, "wb") as ostream:
+        with open(source_path, "r", encoding="utf-8") as istream:
+            for line in istream.readlines():
+                ostream.write(bytes.fromhex("3A" + line[1:]))
 
-with open(output, "wb") as ostream:
-    with open(source, "r", encoding="utf-8") as istream:
-        for line in istream.readlines():
-            ostream.write(bytes.fromhex("3A" + line[1:]))
+if __name__ == "__main__":
+    convert_hex_to_bin(
+        source="pwr-200-wvd-5412-of-pm-New_Firmware.hex",
+        output="output.bin",
+    )
